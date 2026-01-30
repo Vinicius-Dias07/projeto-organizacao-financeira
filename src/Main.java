67aa63119260866daf82bbf66d1dbdad906c419d
Vincius <viniciuscorp07@gmail.com>
2026-01-30 19:03:06 -0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ola estou dando meu primeiro commit no intellij");
+        System.out.println("Testando um novo commit ");
     }
 }
