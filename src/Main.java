bf48bdd3dfb959d4f9232fdbf09532a62c028426
Vincius <viniciuscorp07@gmail.com>
2026-01-30 19:05:47 -0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ola estou dando meu primeiro commit no intellij");
         System.out.println("Testando um novo commit ");
+        System.out.println("Testando o terminal");
     }
 }
